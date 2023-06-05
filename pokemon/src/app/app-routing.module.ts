e38d248fbd897fea46fc8ab31f26920f0da26864
vdcamargo@lopes.com <vdcamargo@lopes.com.br>
2023-06-05 10:39:43 -0300

    save changes

--- pokemon/src/app/app-routing.module.ts
+++ pokemon/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
-import { ModalPokemonModule } from './shared/components/modal-pokemon/modal-pokemon.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { ModalPokemonComponent } from './shared/components/modal-pokemon/modal-pokemon.component';
 
 const routes: Routes = [
   {
